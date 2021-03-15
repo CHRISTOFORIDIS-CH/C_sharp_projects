8e96077247cc1ca58a1ab57d1b2e1122466213bb
Christoforidis Charalampos <61056058+CHRISTOFORIDIS-CH@users.noreply.github.com>
2021-03-15 11:24:52 +0200

    Add files via upload

---
+++ Physics/Physics/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace Physics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(44, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 530);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel2.Location = new System.Drawing.Point(786, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(604, 530);
+            this.panel2.TabIndex = 1;
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(286, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Maroon;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(233, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Maroon;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(773, 665);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(598, 665);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(670, 672);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Maroon;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(861, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "X:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Maroon;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1040, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Y:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(1093, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 32);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(915, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 32);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1458, 744);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Physics/Physics/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Physics
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+        double labelValue;
+        public const double E = 2.7182818284590451;
+        Graphics g;
+        Graphics g2;
+        Pen p;
+        int drawX;
+        int drawY;
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.X.ToString();
+            label2.Text = e.Y.ToString();
+          
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g2 = panel2.CreateGraphics();
+            g = panel1.CreateGraphics();
+            p = new Pen(Color.Black);
+            labelValue = 0;
+            label5.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            if (labelValue == -9)
+            {
+                MessageBox.Show("Cant go above 0.9");
+            }
+            else
+            {
+                labelValue -= 1;
+                if (labelValue < 0)
+                {
+                    label5.Text = "-0." + (-1 * labelValue).ToString();
+                }
+                else {
+                    label5.Text = "0." + labelValue.ToString();
+                }
+            }
+            double x = (1 / Math.Sqrt(1 - Math.Pow(labelValue * 0.1, 2))) * (drawY - labelValue * 0.1 * drawX);
+            double y = (1 / Math.Sqrt(1 - Math.Pow(labelValue * 0.1, 2))) * (drawX - labelValue * 0.1 * drawY);
+            g2.FillRectangle(Brushes.Green, (float)x, (float)y, 10, 10);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (labelValue == 9) {
+                MessageBox.Show("Cant go above 0.9");
+            }
+            else {
+                labelValue += 1;
+                label5.Text = "0." + labelValue.ToString();
+            }
+            double x = (1 / Math.Sqrt(1 - Math.Pow(labelValue * 0.1, 2))) * (drawY - labelValue * 0.1 * drawX);
+            double y = (1 / Math.Sqrt(1 - Math.Pow(labelValue * 0.1, 2))) * (drawX - labelValue * 0.1 * drawY);
+            g2.FillRectangle(Brushes.Green, (float)x, (float)y, 10, 10);
+
+
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+         
+            
+            g.FillRectangle(Brushes.Red, e.X, e.Y, 10, 10);
+            drawX = e.X;
+            drawY = e.Y;
+            double x = (1 / Math.Sqrt(1 - Math.Pow(labelValue * 0.1, 2))) * (drawY - labelValue * 0.1 * drawX);
+            double y = (1 / Math.Sqrt(1 - Math.Pow(labelValue * 0.1, 2))) * (drawX - labelValue * 0.1 * drawY);
+            g2.FillRectangle(Brushes.Green, (float)x, (float)y, 10, 10);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label9.Text = e.X.ToString();
+            label8.Text = e.Y.ToString();
+        }
+    }
+}
